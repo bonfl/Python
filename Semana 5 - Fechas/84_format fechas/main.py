24895cd64a1609869f21e5df707174916d181bb7
Zz902889 <nicolas.bonfiglio@gmail.com>
2019-09-12 23:43:58 -0300

    Ejercicio 84 terminado

--- Semana 5 - Fechas/84_format fechas/main.py
+++ Semana 5 - Fechas/84_format fechas/main.py
@@ -5,4 +5,24 @@ def clear_screen():
 clear_screen()
 
 #Importo libreria de fechas y hs
-import datetime 
+import datetime
+
+now = datetime.datetime.now()
+print(now)
+print()
+now = now.strftime("%B %d, %Y")
+print("La fecha en el nuevo formato es: {}".format(now))
+# linea 14 es igualue hacer un print("la fecha es..",now)
+print()
+
+#si tengo una variable con una fecha en formato cadena, ejemplo:
+fecha_cadena = input("Ingrese fecha: ")
+print ("La fecha en formato cadena es {}".format(fecha_cadena)) #esta fecha en formato cadena no sirve para mucho...
+print()
+#convierto mi string a fecha.
+
+fecha_formateada = datetime.datetime.strptime(fecha_cadena,"%d/%m/%Y")
+print()
+print ("La fecha en formato fecha(convertida del string anterior) es {}".format(fecha_formateada))
+print()
+input("Gracias, presione ENTER para salir...")
