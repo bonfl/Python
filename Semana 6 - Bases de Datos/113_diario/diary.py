279d5534ed93bc00558d5ac7b30b786c97e0ff37
Zz902889 <nicolas.bonfiglio@gmail.com>
2019-09-23 22:11:22 -0300

    Fin semana DB

--- Semana 6 - Bases de Datos/113_diario/diary.py
+++ Semana 6 - Bases de Datos/113_diario/diary.py
@@ -1,24 +1,28 @@
-#Clase desde la 113 hasta la 122
-#Importo librería de peewee
+# Clase desde la 113 hasta la 122
+def clear_screen():
+    import os
+    os.system("clear")
+
+
+clear_screen()
+
+# Importo librería de peewee
 from peewee import *
 
-#traigo libreria de fechas
+# traigo libreria de fechas
 import datetime
 
-#Libreria de diccionarios ordenados
+# Libreria de diccionarios ordenados
 from collections import OrderedDict
 
-#utilidad
+# utilidad
 import sys
 
 # Llamo a mi DB, si no existe, la crea
 db = SqliteDatabase("diary.db")
 
 
-
-
-
-#mi tabla...
+# mi tabla...
 class Entry(Model):
     content = TextField()
     timestamp = DateTimeField(default=datetime.datetime.now)
@@ -29,15 +33,17 @@ class Entry(Model):
 
 def menu_loop():
     """Show menu"""
+    clear_screen()
     choice = None
     while choice != "q":
         print("Press q to quit")
-        for key,value in menu.items():
-            print ("{}) {}".format(key,value.__doc__))
+        for key, value in menu.items():
+            print("{}) {}".format(key, value.__doc__))
         choice = input("Action: ").lower().strip()
 
         if choice in menu:
             menu[choice]()
+
 
 def add_entry():
     """Add Entry"""
@@ -45,33 +51,89 @@ def add_entry():
     data = sys.stdin.read().strip()
 
     if data:
-        if input ("Do you want to save your entry? [Y/n]").lower().strip() != "n":
-            Entry.create(content = data)
+        if input("Do you want to save your entry? [Y/n]").lower().strip() != "n":
+            Entry.create(content=data)
             print("Your Entry was saved successfully")
 
 
-def view_entries():
+def view_entries(search_query=None):
     """View All Entries"""
+    entries = Entry.select().order_by(Entry.timestamp.desc())
 
-def delete_entry():
-        """Delete an Entry"""
+    # para ver si utilizará la búsqueda del search 1 entry
+    if search_query:
+        entries = entries.where(Entry.content.contains(search_query))
+
+    for entry in entries:
+        timestamp = entry.timestamp.strftime("%A %B %d, %Y %I %M%p")
+        print(timestamp)
+        print("+" * len(timestamp))
+        print()
+        print(entry.content)
+        print()
+        print("+" * len(timestamp))
+        print("n) next entry")
+        print("e) edit entry")
+        print("d) delete entry")
+        print("q) Return to menu")
+
+        next_action = input("Action: ").lower().strip()
+        if next_action == "q":
+            break
+        elif next_action == "d":
+            delete_entry(entry)
+        elif next_action == "e":
+            edit_entry(entry)
 
 
-#mi lista ordenada
+def delete_entry(entry):
+    """Delete an Entry"""
+    action = input("Are you sure? y/n").lower().strip()
 
+    if action == "y":
+        entry.delete_instance()
+
+
+def edit_entry(entry):
+    """Edit an Entry"""
+
+    print("Edit your thoughts. Press Ctrl + D to finish")
+    entry_edited = sys.stdin.read().strip()
+    action = input("Are you sure? y/n").lower().strip()
+
+    if action == "y":
+        entry.content = entry_edited
+        entry.save()
+
+
+# guardo texto a buscar
+def search_entries():
+    """Search entries"""
+    search_query = input("Search query: ").strip()
+    view_entries(search_query)
+
+
+# mi lista ordenada
 menu = OrderedDict([
-        ("a",add_entry),
-        ("v",view_entries),
-        ("d",delete_entry)
-    ])
+    ("a", add_entry),
+    ("v", view_entries),
+    ("s", search_entries),
+    ("d", delete_entry)
+])
 
-#Defino función que crea y conect a la DB
+
+# Defino función que crea y conect a la DB
 def create_and_connect():
     """Connects to the DB and create the tables"""
     db.connect()
-    db.create_tables([Entry],safe = True)
+    db.create_tables([Entry], safe=True)
 
-#Este if hace que se ejecute el llamado solo desde python3.7 y no desde otros lados.
+
+# Este if hace que se ejecute el llamado solo desde python3.7 y no desde otros lados.
 if __name__ == '__main__':
     create_and_connect()
     menu_loop()
+
+print()
+input("ENTER to exit...")
+clear_screen()
